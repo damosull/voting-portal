4aef14f27a15a83c63ec30610225c64bca6410a3
Thalles Noce <tnoce@glasslewis.com>
2019-09-09 14:39:15 -0300

    testing another url

--- cypress/integration/user/login.spec.js
+++ cypress/integration/user/login.spec.js
@@ -1,20 +1,36 @@
 /// <reference types="Cypress" />
 
+// describe('Login Page',function(){
+//     it('LogIn with a Internal Admin user',function(){
+//         cy.visit("/");
+
+//         var username = Cypress.env('Internal_Admin_Username');
+//         var userpwd = Cypress.env('Internal_Admin_Password');
+//         cy.get("input#username").type(username).should('have.value',username);
+
+//         cy.get("input#password").type(userpwd);
+//         cy.get("#btn-submit-login").click();
+
+//         //Assert:
+//         //1. Verify if session exists
+//         cy.getCookie('DEV-session').should('exist');
+//         //2. Verify if it lands in the Workflow page
+//         cy.url().should('include','Workflow');
+//     })
+// })
+
 describe('Login Page',function(){
-    it('LogIn with a Internal Admin user',function(){
+    it('Logs in with a test user',function(){
         cy.visit("/");
 
-        var username = Cypress.env('Internal_Admin_Username');
-        var userpwd = Cypress.env('Internal_Admin_Password');
-        cy.get("input#username").type(username).should('have.value',username);
+        var username = Cypress.env('test_user_name');
+        var userpwd = Cypress.env('test_user_password');
+        cy.get("input#LOGIN_USERNAME").type(username).should('have.value',username);
 
-        cy.get("input#password").type(userpwd);
-        cy.get("#btn-submit-login").click();
+        cy.get("#LOGIN_PASSWORD").type(userpwd);
+        cy.get("#signin-button").click();
 
-        //Assert:
-        //1. Verify if session exists
-        cy.getCookie('DEV-session').should('exist');
-        //2. Verify if it lands in the Workflow page
-        cy.url().should('include','Workflow');
+        cy.url().should('include','app.cfm');
+
     })
 })
