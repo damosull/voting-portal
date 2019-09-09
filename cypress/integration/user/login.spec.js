d94e0a4993829f6c81aa66d22a01489351b13700
Thalles Noce <tnoce@glasslewis.com>
2019-09-09 11:15:29 -0300

    first test of cypress on Voting Portal

---
+++ cypress/integration/user/login.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+describe('Login Page',function(){
+    it('LogIn with a Internal Admin user',function(){
+        cy.visit("/");
+
+        var username = Cypress.env('Internal_Admin_Username');
+        var userpwd = Cypress.env('Internal_Admin_Password');
+        cy.get("input#username").type(username).should('have.value',username);
+
+        cy.get("input#password").type(userpwd);
+        cy.get("#btn-submit-login").click();
+
+        //Assert:
+        //1. Verify if session exists
+        cy.getCookie('DEV-session').should('exist');
+        //2. Verify if it lands in the Workflow page
+        cy.url().should('include','Workflow');
+    })
+})
