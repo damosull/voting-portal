765aa4728acc29f7e8afae37585dfb6a991e3592
Denise Desmond <ddesmond@glasslewis.com>
2020-08-07 13:10:54 +0100

    amend code

--- cypress/integration/smoke_tests/external-user-toolbar-search-spec.js
+++ cypress/integration/smoke_tests/external-user-toolbar-search-spec.js
@@ -1,38 +1,50 @@
 /// <reference types="Cypress" />
+
+
 describe('External user', function () {
-  // login       
   beforeEach(function () {
-      cy.server();
-      cy.route('POST', "**/Api/Data/WorkflowExpansion").as('WorkflowExpansion');
-      cy.login(Cypress.env('External_Admin_Username'));
-      cy.visit("/Workflow");
-      cy.wait('@WorkflowExpansion');
+    cy.server();
+    cy.route('POST', "**/Api/Data/WorkflowExpansion").as('WorkflowExpansion');
+    cy.login(Cypress.env('External_Admin_Username'));
+    cy.visit("/Workflow");
+    cy.wait('@WorkflowExpansion');
   })
-  it('Toolbar Search', function () {
+
+  it('Toolbar seach', function () {
+    cy.visit("/");
+   
+    //Assert:
+    //1. Verify if it lands in the Workflow page
+    cy.get('#workflow-link.active', { timeout: 10000 }).should('exist');
+    //2. Verify if session exists
+    cy.getCookie('DEV-session').should('exist');
+    // Search for customer
+    //'California Public Employee Retirement System (CalPERS)'
+   cy.get('#toolbarSearchFieldInput').type("Int");    
+    
+    // check all meetings in response have CalPERS customer id
+    cy.wait('@post').its('response.body.items').each((item) => {
+      expect(item.Summaries.CustomerID.Value).to.equal(196);
+    });
+  });
+});
 
 
-      //Assert:
-      //1. Verify if it lands in the Workflow page
-      cy.get('#workflow-link.active', { timeout: 20000 }).should('exist');
-
-      //2. Verify if session exists
-      cy.getCookie('DEV-session').should('exist');
-
-      cy.server();
-      cy.route('GET', '/Api/Data/ToolbarSearch/').as('get');
+      //cy.server();
+      //cy.route('GET', '/Api/Data/ToolbarSearch/').as('get');
         // Search for customer
         //'California Public Employee Retirement System (CalPERS)'
-        cy.get('#toolbarSearchFieldInput').type("Int");
+    //    cy.get('#toolbarSearchFieldInput').type("Int");
         //cy.get('#kendoCustomers-list .k-item').first().click();
-    cy.route('GET', '/Api/Data/ToolbarSearch/?QueryValue=in&SearchType=0&_=1596799387414/').as('querysearch')
+    //cy.route('GET', '/Api/Data/ToolbarSearch/?QueryValue=in&SearchType=0&_=1596799387414/').as('querysearch')
       //  cy.get('.searchResultContent clearfix toolbar-icon-meetings-blue').first().click();
 
         // check all meetings in response have CalPERS customer id
-        cy.wait('@WorkflowExpansion').its('response.body.items').each((item) => {
-            expect(item.Summaries.CustomerID.Value).to.equal(196);
-        });
-    });
-});
+      //  cy.wait('@WorkflowExpansion').its('response.body.items').each((item) => {
+        //    expect(item.Summaries.CustomerID.Value).to.equal(196);
+  //      });
+//    });
+//});
 
 //cy.get('#toolbarSearchFieldInput').type("CAL");
 //'California Public Employee Retirement System (CalPERS)'
