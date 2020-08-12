df13879dd0ad1d31c8c2ee7c239b8ba3df2b607e
Denise Desmond <ddesmond@glasslewis.com>
2020-08-12 12:51:02 +0100

    Refactored the test

--- cypress/integration/smoke_tests/external-user-toolbar-search-spec.js
+++ cypress/integration/smoke_tests/external-user-toolbar-search-spec.js
@@ -18,12 +18,12 @@ describe('external user', function () {
     // Search for Customer ('Meetings'option is default) & verify user is navigated to correct Meeting Detail page 
     cy.get('#toolbarSearchFieldInput').type("International Breweries plc");
     cy.contains('International Breweries Plc | NG').click();
-    cy.url().should('include', '/MeetingDetails/Index/977763?fromToolbar=true');
+    cy.url().should('include', '/MeetingDetails/Index/');
     cy.get('#company-navigate').should('have.text', 'International Breweries Plc');
 
     // Search for customer ('Companies' option) & verify user is navigated to correct Company page
     cy.get('#toolbarSearchFieldInput').type("international business machines");
-    cy.get('input[id="toolbar-options--companies"]').should('have.value', 'Companies').click();
+    cy.get('#toolbar-options--companies').should('have.value', 'Companies').click();
     cy.contains('International Business Machines Corp. | US').click();
     cy.url().should('include', '/Company/Index/23600');
     cy.get('#md-issuer-name').should('contain', 'International Business Machines Corp.');
