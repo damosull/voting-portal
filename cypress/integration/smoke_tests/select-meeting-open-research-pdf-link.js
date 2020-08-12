7d09ffb0d418f643b7a302e24b5d1d9474f86099
Laura Egan <legan@glasslewis.com>
2020-08-12 15:12:55 +0100

    download PDF file

---
+++ cypress/integration/smoke_tests/select-meeting-open-research-pdf-link.js
@@ -0,0 +1,39 @@
+describe('select a meeting and open research pdf link', function () {
+
+    beforeEach(function () {
+        cy.login(Cypress.env('External_Username'));
+    });
+
+    it(`select meeting and open PDF`, function () {
+        cy.visit('/MeetingDetails/Index/949997');
+
+
+        cy.get('#research-pdf-doc').should('have.attr', 'href', `#PdfResearchReport`)
+            .should('be.visible');
+
+            // if (Cypress.env('configFile') == 'smoke-prod') {
+
+            //          cy.get('#formResearchApiDoc')
+            //              .then((downloadLink) => {
+            //              cy.request('POST', downloadLink).then((resp) => {
+
+        if (Cypress.env('configFile') == 'smoke-prod') {
+
+            cy.get('#research-pdf-doc').invoke('attr', 'href')
+                .then((downloadLink) => {
+                    cy.request('POST', downloadLink).then((resp) => {
+
+                        expect(resp.status).to.eq(200);
+                        //expect(resp.headers).to.have.property('content-disposition').eql('attachment; filename=BallotStatusReport.pdf');
+                        expect(resp.headers).to.have.property('content-type').eql('application/pdf');
+                        expect(resp.headers).to.have.property('content-length').greaterThan(1);
+                        expect(resp.body).include('%PDF');
+
+                        
+                    });
+                });
+        }
+    });
+});
+
+
