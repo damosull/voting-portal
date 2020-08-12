9db79f5591da6566cc8f8c253dd0cae8054fde9a
Jaromir Dinga <jdinga@glasslewis.com>
2020-08-12 15:44:16 +0100

    modifying the report form and downloading report in the same tab

--- cypress/integration/smoke_tests/select-meeting-open-research-pdf-link.js
+++ cypress/integration/smoke_tests/select-meeting-open-research-pdf-link.js
@@ -5,33 +5,37 @@ describe('select a meeting and open research pdf link', function () {
     });
 
     it(`select meeting and open PDF`, function () {
-        cy.visit('/MeetingDetails/Index/949997');
+        cy.visit('/MeetingDetails/Index/980900');
 
 
         cy.get('#research-pdf-doc').should('have.attr', 'href', `#PdfResearchReport`)
             .should('be.visible');
 
-            // if (Cypress.env('configFile') == 'smoke-prod') {
+        // if (Cypress.env('configFile') == 'smoke-prod') {
 
-            //          cy.get('#formResearchApiDoc')
-            //              .then((downloadLink) => {
-            //              cy.request('POST', downloadLink).then((resp) => {
+        //          cy.get('#formResearchApiDoc')
+        //              .then((downloadLink) => {
+        //              cy.request('POST', downloadLink).then((resp) => {
+
+        cy.get('#formResearchApiDoc').invoke('attr', 'target', '').should('have.attr', 'target', '');
+
+        cy.get('#research-html-doc').click();
 
         if (Cypress.env('configFile') == 'smoke-prod') {
 
-            cy.get('#research-pdf-doc').invoke('attr', 'href')
-                .then((downloadLink) => {
-                    cy.request('POST', downloadLink).then((resp) => {
+            // cy.get('#research-pdf-doc').invoke('attr', 'href')
+            //     .then((downloadLink) => {
+            //         cy.request('POST', downloadLink).then((resp) => {
 
-                        expect(resp.status).to.eq(200);
-                        //expect(resp.headers).to.have.property('content-disposition').eql('attachment; filename=BallotStatusReport.pdf');
-                        expect(resp.headers).to.have.property('content-type').eql('application/pdf');
-                        expect(resp.headers).to.have.property('content-length').greaterThan(1);
-                        expect(resp.body).include('%PDF');
+            //             expect(resp.status).to.eq(200);
+            //             //expect(resp.headers).to.have.property('content-disposition').eql('attachment; filename=BallotStatusReport.pdf');
+            //             expect(resp.headers).to.have.property('content-type').eql('application/pdf');
+            //             expect(resp.headers).to.have.property('content-length').greaterThan(1);
+            //             expect(resp.body).include('%PDF');
 
-                        
-                    });
-                });
+
+            //         });
+            //     });
         }
     });
 });
