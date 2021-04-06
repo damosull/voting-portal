537ae589acb6c18f2fac1d8e308af57988a1565d
Patrick Corcoran <pcorcoran@glasslewis.com>
2021-04-06 19:16:34 +0100

    Added automation tests to verify workflow default page elements

---
+++ cypress/integration/workflow/verify-workflow-page-default.js
@@ -0,0 +1,35 @@
+describe('verify workflow page elements (TestCase - 28351)', function () {
+
+    beforeEach(function () {
+        cy.server();
+        cy.route('POST', "**/Api/Data/WorkflowExpansion").as('WorkflowExpansion');
+        cy.route('POST', "**/Api/Data/WorkflowSecuritiesWatchlists").as('WorkflowSecuritiesWatchlists')
+
+        cy.login();
+        cy.visit("/Workflow");
+        cy.wait('@WorkflowExpansion');
+        cy.wait('@WorkflowSecuritiesWatchlists');
+    });
+    it(`verify pre-set filters`, function () {
+
+        cy.get('#btn-date-modal').contains('Next 30 Days').should('be.visible');
+        cy.get('#editorDiv1000 > h4').contains('Number of Ballots > 0').should('be.visible');
+        cy.get('#system-filters').contains('Upcoming Meeting').should('have.class', 'highlightedFilter');
+        cy.get('#my-filters-and-directories > ul').should('be.empty');
+        cy.get('#btn-manage-filters').click();
+        cy.url().should('include', '/ManageFilters');
+        cy.get('#my-filtersand-folders > ul').should('be.empty');
+		
+    });
+	it(`verify filter columns are correctly displayed and in the correct order`, function () {
+
+
+        const filterColumns = ['Company Name','Agenda Key','Policy ID','Control Number','Decision Status','Security Country of Trade','Deadline Date','Meeting Date','Record Date','Meeting Type','Shares','Ballot Blocking'];
+        cy.visit('/Workflow');
+        cy.wait('@WorkflowExpansion');
+        cy.get('#btn-scroll-end').click({ waitForAnimations: false });
+
+        filterColumns.forEach((column) => { cy.get(`th[data-title='${column}']`).scrollIntoView().should('be.visible'); });
+    });
+
+});
