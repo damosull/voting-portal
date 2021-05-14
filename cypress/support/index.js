c0aa8aa0a4d1d454ac63a6cfa92377cc077b11cd
Patrick Corcoran <pcorcoran@glasslewis.com>
2021-05-14 16:58:02 +0100

    adding voting recommendations tests

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -20,5 +20,11 @@ import './commands'
 // require('./commands')
 require('cypress-plugin-retries')
 
+/* eslint-disable no-unused-vars */
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
 
 
