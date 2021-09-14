0e3253f5407927aa7be03b78a088af116a0728f5
Patrick Corcoran <pcorcoran@glasslewis.com>
2021-09-14 08:31:38 +0100

    Adding automated tests for User Story US39255

---
+++ cypress/integration/KanbanUserStories/US39255-TC40724.js
@@ -0,0 +1,100 @@
+//Test Case 40724 - https://dev.azure.com/glasslewis/Development/_workitems/edit/40724
+const { MEETINGID } = require("../../support/constants");
+
+describe('US39255 tests', function () {
+    beforeEach(function () {
+        cy.viewport(1100, 900);
+        cy.intercept('POST', '**/Api/Data/WorkflowExpansion').as('WorkflowExpansion');
+        cy.intercept('POST', '**/Api/Data/WorkflowSecuritiesWatchlists').as('WorkflowSecuritiesWatchlists');
+        cy.intercept('POST', '**/Api/Data/Filters/CreateDraftFilter').as('filter');
+        cy.intercept('POST', '**/Api/Data//MdData/GetAgenda').as('getagenda')
+        cy.intercept('GET', '**/Api/Data/WorkflowResearchInfo/**').as('info')
+        cy.intercept('GET', '**/Api/Data/MeetingDetails/GetFilters**').as('getfilters')
+        cy.intercept('POST', '**/Api/Data/MeetingDetailsActivity/').as('activity')
+        cy.intercept('POST', '**/Api/Data/VoteTally').as('votetally')
+        cy.intercept('GET', '**/Api/Data/MeetingMaterials/**').as('materials')
+
+        cy.loginExtAdm('Russell');
+        cy.visit('/Workflow');
+
+        cy.removeAllExistingSelectedCriteria();
+    });
+
+    it(`Verify Ballot section Pagination`, function () {
+
+        //make sure all dates are current with this meeting id 
+        cy.executeUpdateQuery(`UPDATE PX_Meeting SET
+        MeetingDate = DATEADD(DAY, 10, getdatE()),
+        FileProcessingDate = DATEADD(DAY, -1, getdatE()),
+        HoldReconciliationDate = DATEADD(DAY, 10, getdatE()),
+        LastModifiedDate = DATEADD(DAY, 10, getdatE()),
+        RecordDate = DATEADD(DAY, 10, getdatE()),
+        SharesDependentChangeDate = DATEADD(DAY, 10, getdatE()),
+        VoteDeadlineDate = DATEADD(DAY, 10, getdatE())
+        WHERE MeetingID IN (` + MEETINGID.RLNCDRP + `)`)
+
+        //Step 4 - User Clicks on the valid company in the Workflow page
+        cy.visit('MeetingDetails/Index/' + MEETINGID.RLNCDRP)
+        cy.wait('@getagenda', { timeout: 50000 })
+        //cy.wait('@WorkflowSecuritiesWatchlists');
+        cy.wait('@votetally')
+        cy.wait('@materials')
+
+        //Step 3 - verify the pagination default is set to '10'.
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').invoke('attr', 'style', 'display: block');
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').select('10', { timeout: 50000 })
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > span > span.k-input').then(function (val) {
+            const numBallots = val.text()
+            expect(numBallots).to.equal('10')
+        })
+
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').find(':selected').should('have.text', '10')
+
+        //Step 3 -Now click the pagination dropdown and change the pagination to 50
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').invoke('attr', 'style', 'display: block');
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').select('50', { timeout: 50000 })
+        cy.get('#md-ballots-grid-results').find('tr').its('length').should('eq', 50)
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').find(':selected').should('have.text', '50')
+
+        //Step 5 - Now click the pagination dropdown and change the pagination to 20 and log out of the application.
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').invoke('attr', 'style', 'display: block');
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').select('20', { timeout: 50000 })
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').find(':selected').should('have.text', '20')
+        cy.get('#md-ballots-grid-results').find('tr').its('length').should('eq', 20)
+
+        cy.get('#logged-in-user').click()
+        cy.get('#navlink--logout').click()
+
+
+    })
+    it(`Verify Ballot section Pagination after `, function () {
+
+        //Step 6 - Log back into the voting portal and on Selecting the meeting verify the pagination in the ballot section should be saved as "20".
+        cy.visit('MeetingDetails/Index/' + MEETINGID.RLNCDRP)
+        cy.wait('@getagenda')
+
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > span > span.k-input').then(function (val) {
+            const numBallots = val.text()
+            expect(numBallots).to.equal('20')
+        })
+
+
+        //Step7 - Set pagination to 50 and verify ballot displayed row count
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').invoke('attr', 'style', 'display: block');
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').select('50', { timeout: 50000 })
+        cy.get('#md-ballots-grid-results').find('tr').its('length').should('eq', 50)
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').find(':selected').should('have.text', '50')
+
+        //Step 8 - Now change pagination  to "10" 
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').invoke('attr', 'style', 'display: block');
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').select('10', { timeout: 50000 })
+        cy.get('#ballots-grid > div.k-pager-wrap.k-grid-pager.k-widget > span.k-pager-sizes.k-label > span > select').find(':selected').should('have.text', '10')
+        cy.get('#md-ballots-grid-results').find('tr').its('length').should('eq', 10)
+
+        cy.get('#logged-in-user').click()
+        cy.get('#navlink--logout').click()
+
+
+    })
+
+})
---
+++ cypress/integration/KanbanUserStories/US39255-TC40734.js
@@ -0,0 +1,101 @@
+//TC 40734 - https://dev.azure.com/glasslewis/Development/_workitems/edit/40734
+
+const { MEETINGID } = require("../../support/constants");
+
+describe('US39255 tests', function () {
+    beforeEach(function () {
+        cy.viewport(1100, 900);
+        cy.intercept('POST', '**/Api/Data/WorkflowExpansion').as('WorkflowExpansion');
+        cy.intercept('POST', '**/Api/Data/WorkflowSecuritiesWatchlists').as('WorkflowSecuritiesWatchlists');
+        cy.intercept('POST', '**/Api/Data/Filters/CreateDraftFilter').as('filter');
+        cy.intercept('POST', '**/Api/Data//MdData/GetAgenda').as('getagenda')
+        cy.intercept('GET', '**/Api/Data/WorkflowResearchInfo/**').as('info')
+        cy.intercept('GET', '**/Api/Data/MeetingDetails/GetFilters**').as('getfilters')
+        cy.intercept('POST', '**/Api/Data/MeetingDetailsActivity/').as('activity')
+        cy.intercept('GET', '**/Api/Data/Inbox/**').as('inbox')
+        cy.intercept('POST', '**/Api/Data/VoteTally').as('votetally')
+
+        cy.loginExtAdm('Neuberger');
+        cy.visit('/Workflow');
+        cy.wait('@WorkflowExpansion');
+        cy.wait('@WorkflowSecuritiesWatchlists');
+
+        cy.removeAllExistingSelectedCriteria();
+    });
+
+    it(`Verify User can Toggle between 'Management' Multiple Agendas in the Vote card page for specific meeting type`, function () {
+
+        //make sure all dates are current with this meeting id 
+        cy.executeUpdateQuery(`UPDATE PX_Meeting SET
+        MeetingDate = DATEADD(DAY, 10, getdatE()),
+        FileProcessingDate = DATEADD(DAY, -1, getdatE()),
+        HoldReconciliationDate = DATEADD(DAY, 10, getdatE()),
+        LastModifiedDate = DATEADD(DAY, 10, getdatE()),
+        RecordDate = DATEADD(DAY, 10, getdatE()),
+        SharesDependentChangeDate = DATEADD(DAY, 10, getdatE()),
+        VoteDeadlineDate = DATEADD(DAY, 10, getdatE())
+        WHERE MeetingID IN (` + MEETINGID.NBCOMMO + `)`)
+
+        //Step 3 - User Clicks on the valid company in the Workflow page
+        cy.visit('MeetingDetails/Index/' + MEETINGID.NBCOMMO)
+        cy.wait('@getagenda')
+
+
+        //Step 4 - Click on the 'Management' vote card dropdown
+        cy.get('#agendas-list > ul > li').invoke('attr', 'class', 'dropdown related-meetings-list open')
+
+        //Step 5 - Verify 'Ballots' section will only display specific 'Management' agenda type ballot details [Eg : Management - 934050888] 
+        cy.get('#agendas-list > ul > li > ul > li > div > span').then(function (val) {
+            const agenda = val.text()
+            expect(agenda).to.include(MEETINGID.NBCOMMO_AGENDA1)
+        })
+        cy.get('#md-ballots-grid-results > tr > td > a').then(function (ctrlnum) {
+            const displayedCtrlNum = ctrlnum.text()
+            expect(displayedCtrlNum).to.include(MEETINGID.NBCOMMO_CTRLNUM1)
+        })
+
+        //Step 6 - Select another 'Management' Vote card in the dropdown list[Eg: Management -934050915]
+        //Expected - Vote Card page gets refreshed and 'Ballots' section gets updated with the 'Agenda Type' as 'Management' and 'Ballot Control Number' as different to that of previous 'Management' number
+        cy.get('#agendas-list > ul > li > ul > li:nth-child(3) > a > span').click()
+        cy.wait('@getagenda')
+        cy.wait('@info')
+        cy.wait('@getfilters')
+        cy.wait('@activity')
+        cy.wait('@inbox')
+        cy.wait('@votetally')
+
+        cy.get('#agendas-list > ul > li > ul > li > div > span').then(function (val) {
+            const agenda = val.text()
+            expect(agenda).to.include(MEETINGID.NBCOMMO_AGENDA2)
+        })
+        cy.get('#md-ballots-grid-results > tr > td > a').then(function (ctrlnum2) {
+            const displayedCtrlNum = ctrlnum2.text()
+            expect(displayedCtrlNum).to.include(MEETINGID.NBCOMMO_CTRLNUM2)
+        })
+
+        //verify all agendas can can be listed
+        cy.get('#agendas-list > ul > li').invoke('attr', 'class', 'dropdown related-meetings-list open')
+        cy.get('#agendas-list > ul > li > ul > li:nth-child(4) > a > span').click()
+        cy.wait('@getagenda')
+        cy.wait('@info')
+        cy.wait('@getfilters')
+        cy.wait('@activity')
+        cy.wait('@inbox')
+        cy.wait('@votetally')
+
+
+        cy.get('#agendas-list > ul > li > ul > li > div > span').then(function (val) {
+            const agenda = val.text()
+            expect(agenda).to.include(MEETINGID.NBCOMMO_AGENDA3)
+        })
+        cy.get('#md-ballots-grid-results > tr > td > a').then(function (ctrlnum3) {
+            const displayedCtrlNum1 = ctrlnum3.text()
+            expect(displayedCtrlNum1).to.include(MEETINGID.NBCOMMO_CTRLNUM3)
+        })
+
+
+    })
+})
+
+
+
