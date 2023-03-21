02307204dc8008769af26f433204a1aad31b1cee
Sagar Maheshwari <smaheshwari@glasslewis.com>
2023-03-21 17:19:38 +0000

    fix packages

--- cypress.config.js
+++ cypress.config.js
@@ -87,7 +87,7 @@ async function setupNodeEvents(on, config) {
 }
 
 module.exports = defineConfig({
-  projectId: 'testproject',
+	projectId: 'testproject',
 	defaultCommandTimeout: 30000,
 	requestTimeout: 15000,
 	responseTimeout: 30000,
