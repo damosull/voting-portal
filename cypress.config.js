2f50befdd691eb00677a56d09b5b64aa3f6d80b4
Sagar Maheshwari <smaheshwari@glasslewis.com>
2023-04-06 15:48:22 +0100

    Enable Memory Management

--- cypress.config.js
+++ cypress.config.js
@@ -93,7 +93,7 @@ module.exports = defineConfig({
 	pageLoadTimeout: 60000,
 	numTestsKeptInMemory: 2,
 	chromeWebSecurity: false,
-	experimentalWebKitSupport: false,
+	experimentalMemoryManagement: true,
 	screenshotsFolder: 'test-results/screenshots',
 	videosFolder: 'test-results/videos',
 	viewportWidth: 1920,
