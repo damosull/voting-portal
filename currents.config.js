d0c6bc641e0be58192855675727fa4072e41fc2f
Sagar Maheshwari <smaheshwari@glasslewis.com>
2023-04-20 15:33:50 +0100

    replace director url

--- currents.config.js
+++ currents.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	projectId: 'voting-portal', // the projectId, can be any values for sorry-cypress users
 	recordKey: 'xxx', // the record key, can be any value for sorry-cypress users
-	cloudServiceUrl: 'http://localhost:1234', // Sorry Cypress users - set the director service URL
+	cloudServiceUrl: 'https://sorrycypress-director.devops.glasslewis.net', // Sorry Cypress users - set the director service URL
 };
 
 //run command - npx cypress-cloud run --parallel --record --key XXX --config specPattern=cypress/e2e/SmokeTests/*.feature --ci-build-id 123
