21cd35ac5f5b2849fa2037d98c681e089faff418
Sagar Maheshwari <smaheshwari@glasslewis.com>
2023-04-20 16:06:55 +0100

    amend key

--- currents.config.js
+++ currents.config.js
@@ -4,4 +4,4 @@ module.exports = {
 	cloudServiceUrl: 'https://sorrycypress-director.devops.glasslewis.net', // Sorry Cypress users - set the director service URL
 };
 
-//run command - npx cypress-cloud run --parallel --record --key XXX --config specPattern=cypress/e2e/SmokeTests/*.feature --ci-build-id 123
+//run command - npx cypress-cloud run --parallel --record --key xxx --config specPattern=cypress/e2e/SmokeTests/*.feature --ci-build-id 123
