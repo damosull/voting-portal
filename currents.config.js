8af81fc1fc45da0aa9e15eadfb9c479369776abe
Sagar Maheshwari <smaheshwari@glasslewis.com>
2023-06-09 17:00:22 +0100

    revert director URL change

--- currents.config.js
+++ currents.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	projectId: 'voting-portal',
 	recordKey: 'xxx',
-	cloudServiceUrl: 'https://sorrycypress-director-devops.glasslewis.net',
+	cloudServiceUrl: 'https://sorrycypress-director.devops.glasslewis.net',
 };
